350f5ba830e413991311687f2c76fa5bc2b24f9c
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-04-09 17:27:00 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,38 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
 public class PracticeProblem {
 
+	public static int[] recaman(int n) {
+		if (n <= 0) {
+			return new int[0];
+		}
+		int[] array = new int[n];
+		Set<Integer> set = new HashSet<>();
+		recamanHelper(1, 0, array, set, n);
+		return array;
+	}
+
+	private static void recamanHelper(int i, int prev, int[] array, Set<Integer> set, int n) {
+		if (i > n) {
+			return;
+		}
+		int next = prev - i;
+		if (next <= 0 || set.contains(next)) {
+			next = prev + i;
+		}
+		array[i - 1] = next;
+		set.add(next);
+		recamanHelper(i + 1, next, array, set, n);
+	}
+	
 	public static void main(String args[]) {
+		int[] result = recaman(6);
+		System.out.println(Arrays.toString(result));
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
+	
 
 }
